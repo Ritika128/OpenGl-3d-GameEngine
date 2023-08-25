e6662af4cf34d0978c09de3f1c5926d4d18e6e93
Anish Mahadevan <anishmahadevan1772@gmail.com>
2023-08-25 16:26:35 +0530

    Updating code files

--- Tut_Programs/src/main.cpp
+++ Tut_Programs/src/main.cpp
@@ -25,6 +25,15 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        //Code to render a triangle using Legacy OpenGL
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.0f,  0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glEnd();
+
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
