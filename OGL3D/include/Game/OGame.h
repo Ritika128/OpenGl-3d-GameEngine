6303758f34c924e967ef17471b18c92a7b51e0fb
Ritika Nankar <112073087+Ritika128@users.noreply.github.com>
2023-09-05 22:42:07 +0530

    Create OGame.h

---
+++ OGL3D/include/Game/OGame.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <memory>
+#include <OGL3D/Graphics/OGraphicsEngine.h>
+class OWindow;
+class OGame
+{
+public:
+	OGame();
+	~OGame();
+
+	virtual void onCreate();
+	virtual void onUpdate();
+	virtual void onQuit();
+
+	void run();
+	void quit();
+protected:
+	bool m_isRunning = true;	
+	std::unique_ptr<OGraphicsEngine> m_graphicsEngine;
+	std::unique_ptr<OWindow> m_display;	
+};
