2b6101d3abc94f45d63609005374991d3a357f6c
Ritika Nankar <112073087+Ritika128@users.noreply.github.com>
2023-09-08 23:03:12 +0530

    Create modern_triangle.cpp

---
+++ Render/modern_triangle.cpp
@@ -0,0 +1,133 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    //error handling code, iv integer the specifies it wants a vector
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS,&result);
+    if(result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id,GL_INFO_LOG_LENGTH,&length);
+        char* message = (char*)alloca(length* sizeof(char));
+        glGetShaderInfoLog(id,length,&length,message);
+        std::cout<< "Failed to compile " << 
+        (type == GL_VERTEX_SHADER ? "vertex" : "fragment")
+        << std::endl;
+        std::cout<< message<< std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+
+    return id;
+}
+
+//creating shader
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+    //to link vs and fs together
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+    //to delete when we get the results
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f, 0.5f,
+         0.5f, -0.5f
+         };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);                                                    // This function is generating a buffer and giving us back an id
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);                                       // This function is used to select(bind) a buffer
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); // This function is used to provide data to the buffer
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT,GL_FALSE, sizeof(float) * 2,0);
+
+    std::string vertexShader = 
+        "#version 120\n" 
+        "\n" 
+        "attribute vec4 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        " gl_Position = position;\n"
+        "}\n";
+
+    std::string fragmentShader = 
+        "#version 120\n" 
+        "\n" 
+        "void main()\n"
+        "{\n"
+        " gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    unsigned int shader = CreateShader(vertexShader,fragmentShader);
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    glUseProgram(shader);
+    
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES,0,3);
+
+        
+
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+    glDeleteProgram(shader);
+
+    glfwTerminate();
+    return 0;
+}
