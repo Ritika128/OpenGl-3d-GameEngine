0ea7ee951c93893926b2321813fa91ae9cebfeca
Ritika Nankar <112073087+Ritika128@users.noreply.github.com>
2023-09-08 23:02:18 +0530

    Update and rename modern_triangle.cpp to modern_rectangle.cpp

--- Render/modern_rectangle.cpp
+++ Render/modern_rectangle.cpp
@@ -56,11 +56,6 @@ int main(void)
     if (!glfwInit())
         return -1;
 
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
